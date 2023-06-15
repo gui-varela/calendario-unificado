f7859c66034384f51f97931101df1bcc77f08455
Guilherme Varela <guilhermevareladossantos@gmail.com>
2023-06-15 16:52:33 -0300

    Cria verificação se usuário é Aluno
    Para recuperar o aluno por um id de usuário, é necessário que esse usuário tenha perfil de aluno

--- src/modules/perfis/aluno/aluno.service.ts
+++ src/modules/perfis/aluno/aluno.service.ts
@@ -7,16 +7,30 @@ export class AlunoService {
   constructor(private prisma: PrismaService) {}
 
   async findAlunoPorUsuarioId(usuarioId: string) {
-    const usuario = await this.prisma.aluno.findUniqueOrThrow({
+    const aluno = await this.prisma.aluno.findUniqueOrThrow({
       where: {
         usuarioId,
       },
     });
 
-    if (!usuario) {
+    if (!aluno) {
       throw new AppError('Usuário não existe');
     }
 
-    return usuario;
+    const usuario = await this.prisma.usuario.findUniqueOrThrow({
+      where: {
+        id: usuarioId,
+      },
+    });
+
+    const ID_PERFIL_ALUNO = 'd75a712e-1879-4da7-a4ac-f9950fc239f4';
+
+    const isUsuarioAluno = usuario.perfilId === ID_PERFIL_ALUNO;
+
+    if (!isUsuarioAluno) {
+      throw new AppError('Este usuário não tem perfil de aluno');
+    }
+
+    return aluno;
   }
 }
